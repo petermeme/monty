77420f611f0f4576a5f69783e47ec82f72784ece
petermeme <petermeme90@gmail.com>
2023-08-27 19:54:59 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include "monty.h"
 
-stack_t **global_head;
+stack_t **global_head = NULL;
 
 /**
  * main - the root of the project
@@ -27,4 +28,49 @@ int main(int argc, char *argv[])
 	atexit(global_free);
 
 	exit(EXIT_SUCCESS);
+}
+
+
+/**
+ * read_file - reads a file
+ * @stack: pointer to file descriptor
+ * @file: pathname to file
+ * Return: void
+ */
+
+void read_file(char *file, stack_t **stack)
+{
+	size_t len;
+	int read;
+	unsigned int num = 0;
+	char *line;
+	FILE *fd;
+	char *command;
+
+	fd = fopen(file, "r");
+	if (!fd)
+	{
+		printf("Error: Can't open file %s\n", file);
+		exit(EXIT_FAILURE);
+	}
+	puts ("enter text");
+	line = (char *) malloc(len + 1);
+	read = getline(&line, &len, fd);
+
+	if (read == -1)
+	{
+		command = strtok(line, delims);
+		num++;
+	}
+		if (command)
+		{
+			parse_command(stack, command, num);
+		}
+
+	if (line)
+	{
+		free(line);
+	}
+
+	fclose(fd);
 }
